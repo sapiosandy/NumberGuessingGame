53856fefa6e0ab48a88a5c0cafa92c42923507de
Sandy Gomez <sgomez1029@gmail.com>
2025-06-11 14:56:20 -0700

    Initial Commit

---
+++ NumberGuessingGame/NumberGuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberGuessingGameApp.swift
+//  NumberGuessingGame
+//
+//  Created by Sandra Gomez on 6/11/25.
+//
+
+import SwiftUI
+
+@main
+struct NumberGuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
