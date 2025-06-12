021488bf001debe0b486ee35c321b7d155316bb8
Sandy Gomez <sgomez1029@gmail.com>
2025-06-12 14:44:11 -0700

    Finished Guessing Game app

--- NumberGuessingGame/ContentView.swift
+++ NumberGuessingGame/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     @State private var guess = ""
     @State private var showingAlert = false
     @State private var numberOfTries = 0
-    
+    @State private var alertMessage = ""
     
     var body: some View {
         VStack {
@@ -21,14 +21,38 @@ struct ContentView: View {
                 .border(Color.gray)
                 .padding()
             
-            Button("Submit Guess"){
-            
+            Button("Submit Guess") {
+                if let guessedNumber = Int(guess) {
+                    numberOfTries += 1
+                    if guessedNumber < number {
+                        alertMessage = "Too low! Try again."
+                    } else if guessedNumber > number {
+                        alertMessage = "Too high! Try again."
+                    } else {
+                        alertMessage = "Congratulations! You guessed the number in \(numberOfTries) tries."
+                    }
+                } else {
+                    alertMessage = "Please enter a valid number"
                 }
+                showingAlert = true
             }
             .buttonStyle(.borderedProminent)
             .tint(.green)
         }
+        .alert("Number Guessing Game", isPresented: $showingAlert) {
+            Button("Play Again") {
+                number = Int.random(in: 1...20)
+                numberOfTries = 0
+                guess = ""
+            }
+            Button("OK", role: .cancel) {
+                
+            }
+        } message : {
+            Text(alertMessage)
+        }
     }
-#Preview {
-    ContentView()
 }
+    #Preview {
+        ContentView()
+    }
